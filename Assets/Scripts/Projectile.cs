63807c7a77fd7dd9b7ae747319e5aa2902b32661
M.N <nmg1033@gmail.com>
2021-09-28 00:11:19 +0300

    Second commit

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public LayerMask collisionMask;
     float speed = 10;
     
     public void SetSpeed(float newSpeed) 
@@ -14,6 +15,16 @@ public class Projectile : MonoBehaviour
     void Update()
     {
         float moveDistance = speed*Time.deltaTime;
+        CheckCollisions (moveDistance);
         transform.Translate(Vector3.forward*moveDistance);
     }
+
+    void CheckCollisions (float moveDistance) 
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, moveDistance, collisionMask))
+    }
 }
+//dsadasdsadasda
